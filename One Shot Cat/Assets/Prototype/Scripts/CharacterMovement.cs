c4190d35e6dc5323aadca97b6f580c774ce77546
Iris <31053695+AlgoritmoAlgoritmo@users.noreply.github.com>
2024-10-06 07:46:42 -0300

    Unity project initialization push

---
+++ One Shot Cat/Assets/Prototype/Scripts/CharacterMovement.cs
@@ -0,0 +1,22 @@
+/*
+* Author: Iris Bermudez
+* GitHub: https://github.com/AlgoritmoAlgoritmo
+* Date: DD/MM/YYYY (DD/MM/YY)
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterMovement : MonoBehaviour {
+	#region Variables
+	#endregion
+
+	#region Public methods
+	#endregion
+
+	#region Private methods
+	#endregion
+}
