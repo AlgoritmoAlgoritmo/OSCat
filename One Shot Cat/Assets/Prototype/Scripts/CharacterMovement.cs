0e403819f5715a9a8c1abd8dc935ca91c4adcbc3
Iris <31053695+AlgoritmoAlgoritmo@users.noreply.github.com>
2024-10-06 23:43:19 -0300

    Character movement
    - Tried different Unity versions
    - Implemented character physics and movement

--- One Shot Cat/Assets/Prototype/Scripts/CharacterMovement.cs
+++ One Shot Cat/Assets/Prototype/Scripts/CharacterMovement.cs
@@ -1,22 +1,46 @@
 /*
 * Author: Iris Bermudez
 * GitHub: https://github.com/AlgoritmoAlgoritmo
-* Date: DD/MM/YYYY (DD/MM/YY)
+* Date: 06/10/2024 (DD/MM/YY)
 */
 
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
-public class CharacterMovement : MonoBehaviour {
-	#region Variables
-	#endregion
+namespace OneShotCat.Prototype {
+	[RequireComponent(typeof(Rigidbody))]
+	[RequireComponent( typeof( PlayerInput ) )]
+	public class CharacterMovement : MonoBehaviour {
+		#region Variables
+		[SerializeField]
+		private float speed = 1f;
+		public float Speed { get; set; }
 
-	#region Public methods
-	#endregion
+		[SerializeField]
+		private string actionName = "Move";
 
-	#region Private methods
-	#endregion
+
+		private  Rigidbody rigidBody;
+		private PlayerInput playerInput;
+		#endregion
+
+		#region Public methods
+		private void Awake() {
+			rigidBody = GetComponent<Rigidbody>();
+			playerInput = GetComponent<PlayerInput>();
+		}
+
+        private void FixedUpdate() {
+			Move( playerInput.actions[actionName].ReadValue<Vector2>() );
+		}
+        #endregion
+
+        #region Private methods
+        private void Move( Vector3 _direction ) {
+			rigidBody.velocity = _direction * speed;
+		}
+		#endregion
+	}
 }
