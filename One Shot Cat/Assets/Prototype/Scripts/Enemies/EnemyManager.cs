ab25de0f15f940c61ac1e05881d662c1cc970aca
Iris <31053695+AlgoritmoAlgoritmo@users.noreply.github.com>
2024-10-09 14:34:21 -0300

    Implemented automatic enemy spawn

--- One Shot Cat/Assets/Prototype/Scripts/Enemies/EnemyManager.cs
+++ One Shot Cat/Assets/Prototype/Scripts/Enemies/EnemyManager.cs
@@ -13,22 +13,30 @@ using UnityEngine.AI;
 namespace OneShotCat.Prototype {
 	public class EnemyManager : MonoBehaviour {
         #region Variables
-        [SerializeField]
-        private List<NavMeshAgent> enemiesNavMeshAgents = new List<NavMeshAgent>();
+        [Header( "General variables" )]
 		[SerializeField]
 		private Transform playerTransform;
+        [SerializeField]
+        private List<NavMeshAgent> enemiesNavMeshAgents = new List<NavMeshAgent>();
+
+
+        [Header("Enemy spawn variables")]
+        [SerializeField]
+        private int maxEnemyAmount = 2;
+        public int MaxEnemyAmount { get; set; }
+
+        [SerializeField]
+        private GameObject enemyPrefab;
+        [SerializeField]
+        private List<Transform> spawningPoints = new List<Transform>();
+
 
         private List<GameObject> enemiesToDestroy = new List<GameObject>();
         #endregion
 
         #region MonoBehaviour methods
         private void FixedUpdate() {
-            foreach( NavMeshAgent agent in enemiesNavMeshAgents ) {
-                if( agent.isActiveAndEnabled )
-                    agent.destination = playerTransform.position;
-            }
-
-            CheckEnemiesState();
+            UpdateEnemiesState();
         }
         #endregion
 
@@ -39,11 +47,37 @@ namespace OneShotCat.Prototype {
             _enemyGameObject.gameObject.SetActive( false );
         }
 
-        public void CheckEnemiesState() {
+        public void UpdateEnemiesState() {
+            RemoveDestroyedEnemies();
+            UpdatePlayerPosition();
+            AddNewEnemy();
+        }
+        #endregion
+
+
+        #region Private methods
+        private void UpdatePlayerPosition() {
+            foreach( NavMeshAgent agent in enemiesNavMeshAgents ) {
+                if( agent.isActiveAndEnabled )
+                    agent.destination = playerTransform.position;
+            }
+        }
+
+
+        private void RemoveDestroyedEnemies() {
             for( int i = 0; enemiesToDestroy.Count > 0; i++ ) {
                 enemiesNavMeshAgents.Remove( enemiesToDestroy[0].GetComponent<NavMeshAgent>() );
                 GameObject.Destroy( enemiesToDestroy[0], 2f );
                 enemiesToDestroy.RemoveAt( 0 );
+            }
+        }
+
+        private void AddNewEnemy() {
+            if( enemiesNavMeshAgents.Count < maxEnemyAmount ) {
+                enemiesNavMeshAgents.Add(   GameObject.Instantiate( enemyPrefab,
+                                                spawningPoints[Random.Range( 0, spawningPoints.Count )].position,
+                                                Quaternion.identity )
+                                            .GetComponent<NavMeshAgent>() );                
             }
         }
         #endregion
